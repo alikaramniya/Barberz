ce89b1e02a5439d751c79b037079c83ed955c4ed
alikaram <alikaramniyabadreh98@gmail.com>
2024-05-31 11:16:02 +0330

    اضافه کردن مسیر لیست کاربران

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Admin Route
+Route::group(
+    [
+        'as' => 'admin.',
+        'prefix' => 'admin',
+        'middleware' => ['auth']
+    ], function() {
+    // User Route 
+        Route::group(
+            [
+                'as' => 'user.',
+                'prefix' => 'user',
+                'controller' => UserController::class
+            ], function() {
+                Route::get('index', 'index')->name('index');
+        });
 });
 
 Route::get('/dashboard', function () {
